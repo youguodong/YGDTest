17a1285fb839b54e7bdf67ba7d2b9f28ecfdd59a
youguodong <1046936650@qq.com>
2018-10-23 15:48:15 +0800

    cc

---
+++ Test/YGDTest.h
@@ -0,0 +1,17 @@
+//
+//  YGDTest.h
+//  interview
+//
+//  Created by 尤国栋 on 2018/10/23.
+//  Copyright © 2018年 尤国栋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YGDTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test/YGDTest.m
@@ -0,0 +1,13 @@
+//
+//  YGDTest.m
+//  interview
+//
+//  Created by 尤国栋 on 2018/10/23.
+//  Copyright © 2018年 尤国栋. All rights reserved.
+//
+
+#import "YGDTest.h"
+
+@implementation YGDTest
+
+@end
